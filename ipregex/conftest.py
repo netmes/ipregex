faee254d85b41c03f97a2d9175d1da20bf93accf
Omer Ozarslan <omer@utdallas.edu>
2018-05-14 11:09:31 -0500

    Modify reason message

--- ipregex/conftest.py
+++ ipregex/conftest.py
@@ -10,7 +10,7 @@ def pytest_addoption(parser):
 def pytest_collection_modifyitems(config, items):
     if config.getoption("--runslow"):
         return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    skip_slow = pytest.mark.skip(reason="Need --runslow option to run.")
     for item in items:
         if "slow" in item.keywords:
             item.add_marker(skip_slow)
