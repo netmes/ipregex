4b86e3586676fe189534925a32e242c1b7019418
Omer Ozarslan <omer@utdallas.edu>
2018-05-14 11:08:35 -0500

    Change steps back to 1 in exhaust IPv4 test

--- ipregex/test_ipregex.py
+++ ipregex/test_ipregex.py
@@ -4,15 +4,36 @@ from . import (
 )
 import pytest
 
+
 @pytest.mark.slow
+def test_ipv4_exhaust_correct_addresses():
+    regex = IPv4Regex().regex
+
+    for i in range(0, 255, 1):
+        for j in range(0, 255, 1):
+            for k in range(0, 255, 1):
+                for m in range(0, 255, 1):
+                    assert(regex.match('%s.%s.%s.%s' % (i, j, k, m)))
+
+
 def test_ipv4_correct_addresses():
     regex = IPv4Regex().regex
 
-    for i in range(0, 255, 2):
-        for j in range(0, 255, 3):
-            for k in range(0, 255, 5):
-                for m in range(0, 255, 7):
-                    assert(regex.match('%s.%s.%s.%s' % (i, j, k, m)))
+    assert(regex.match('0.0.0.0') != None)
+    assert(regex.match('1.1.1.1') != None)
+    assert(regex.match('11.11.11.11') != None)
+    assert(regex.match('111.111.111.111') != None)
+    assert(regex.match('222.222.222.222') != None)
+    assert(regex.match('250.250.250.250') != None)
+    assert(regex.match('255.255.255.255') != None)
+
+    assert(regex.match('1.11.111.222') != None)
+    assert(regex.match('222.111.11.1') != None)
+    assert(regex.match('1.11.111.250') != None)
+    assert(regex.match('250.111.11.1') != None)
+    assert(regex.match('1.11.222.250') != None)
+    assert(regex.match('250.222.11.1') != None)
+
 
 def test_ipv4_incorrect_addresses():
     regex = IPv4Regex().regex
@@ -31,6 +52,9 @@ def test_ipv4_incorrect_addresses():
     assert(regex.match('...1')      == None)
     assert(regex.match('1..')       == None)
 
+    assert(regex.match('1,1,1,1')   == None)
+    assert(regex.match('1-1-1-1')   == None)
+
     assert(regex.match('256.1.1.1') == None)
     assert(regex.match('1.256.1.1') == None)
     assert(regex.match('1.1.256.1') == None)
@@ -41,4 +65,3 @@ def test_ipv4_incorrect_addresses():
     assert(regex.match('`1.1.1.1') == None)
     assert(regex.match('1.1.a1.1') == None)
     assert(regex.match('1.1.1.1a') == None)
-
