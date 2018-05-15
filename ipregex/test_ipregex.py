3e4a72c8300c8b2a9ae5ea875144f7fc5503c8b1
Omer Ozarslan <omer@utdallas.edu>
2018-05-15 17:36:16 -0500

    Add more tests

--- ipregex/test_ipregex.py
+++ ipregex/test_ipregex.py
@@ -3,6 +3,7 @@ from . import (
     IPv6Regex
 )
 import pytest
+import itertools
 
 
 @pytest.mark.slow
@@ -14,6 +15,19 @@ def test_ipv4_exhaust_correct_addresses():
             for k in range(0, 255, 1):
                 for m in range(0, 255, 1):
                     assert(regex.match('%s.%s.%s.%s' % (i, j, k, m)))
+
+def test_ipv4_boundary_segments():
+    regex = IPv4Regex().regex
+
+    correct_segments = [
+        '0', '9', '10', '99', '100', '199', '200', '249', '250', '255'
+    ]
+
+    correct_ip_addresses = itertools.product(correct_segments, repeat=4)
+
+    for ip in correct_ip_addresses:
+        ip = '.'.join(ip)
+        assert(regex.match(ip) != None)
 
 
 def test_ipv4_correct_addresses():
@@ -66,5 +80,38 @@ def test_ipv4_incorrect_addresses():
     assert(regex.match('1.1.a1.1') == None)
     assert(regex.match('1.1.1.1a') == None)
 
-def test_ipv6_incorrect_addresses():
-    pass
+def test_ipv6_correct_addresses():
+    regex = IPv6Regex().regex
+
+    assert(regex.match('0000:0000:0000:0000:0000:0000:0000:0000'))
+    assert(regex.match('1111:1111:1111:1111:1111:1111:1111:1111'))
+    assert(regex.match('2222:2222:2222:2222:2222:2222:2222:2222'))
+    assert(regex.match('3333:3333:3333:3333:3333:3333:3333:3333'))
+    assert(regex.match('4444:4444:4444:4444:4444:4444:4444:4444'))
+    assert(regex.match('5555:5555:5555:5555:5555:5555:5555:5555'))
+    assert(regex.match('6666:6666:6666:6666:6666:6666:6666:6666'))
+    assert(regex.match('7777:7777:7777:7777:7777:7777:7777:7777'))
+    assert(regex.match('8888:8888:8888:8888:8888:8888:8888:8888'))
+    assert(regex.match('9999:9999:9999:9999:9999:9999:9999:9999'))
+    assert(regex.match('aaaa:aaaa:aaaa:aaaa:aaaa:aaaa:aaaa:aaaa'))
+    assert(regex.match('bbbb:bbbb:bbbb:bbbb:bbbb:bbbb:bbbb:bbbb'))
+    assert(regex.match('cccc:cccc:cccc:cccc:cccc:cccc:cccc:cccc'))
+    assert(regex.match('dddd:dddd:dddd:dddd:dddd:dddd:dddd:dddd'))
+    assert(regex.match('eeee:eeee:eeee:eeee:eeee:eeee:eeee:eeee'))
+    assert(regex.match('ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff'))
+
+    # Collapsed versions
+    assert(regex.match('::1111:1111:1111:1111:1111:1111:1111'))
+    assert(regex.match('1111::1111:1111:1111:1111:1111:1111'))
+    assert(regex.match('1111:1111::1111:1111:1111:1111:1111'))
+    assert(regex.match('1111:1111:1111::1111:1111:1111:1111'))
+    assert(regex.match('1111:1111:1111:1111::1111:1111:1111'))
+    assert(regex.match('1111:1111:1111:1111:1111::1111:1111'))
+    assert(regex.match('1111:1111:1111:1111:1111:1111::1111'))
+    assert(regex.match('1111:1111:1111:1111:1111:1111:1111::'))
+    assert(regex.match('::'))
+
+    # With leading zeros
+
+    # Without leading zeros
+    assert(regex.match('0:0:0:0:0:0:0:0'))
