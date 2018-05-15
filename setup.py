baab2e3f1d87fd6f97fc56e200f0cb7725854b15
Omer Ozarslan <omer@utdallas.edu>
2018-05-15 17:37:29 -0500

    Bump version 0.1.0 -> 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ipregex',
-    version='0.1.0',
+    version='0.1.1',
     author='Omer F. Ozarslan',
     author_email='omer@utdallas.edu',
     packages=['ipregex'],
