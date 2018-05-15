3ec4ae9cc71ca6d10e737f65785e3f3025b3f807
Omer Ozarslan <omer@utdallas.edu>
2018-05-15 11:50:53 -0500

    Update LICENSE field and classifiers in setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,20 @@ setup(
     author_email='omer@utdallas.edu',
     packages=['ipregex'],
     url='http://github.com/netmes/ipregex/',
-    license='LICENSE',
+    license='GPLv3',
     description=('Provides regular expression for IPv4/IPv6 addresses.'),
     long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: System :: Networking',
+    ],
 )
 
