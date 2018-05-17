dfcac32b46363030a0538fcb0a3c68410c9df7df
Omer Ozarslan <omer@utdallas.edu>
2018-05-17 12:02:39 -0500

    Create a separate version file and fix license

--- ipregex/__init__.py
+++ ipregex/__init__.py
@@ -1,3 +1,7 @@
+from .__version__ import (
+    __title__, __description__, __url__, __version__, __author__,
+    __author_email__, __license__, __copyright__
+)
 import re
 
 class IPRegexBase(object):
--- setup.py
+++ setup.py
@@ -1,25 +1,40 @@
 from setuptools import setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, 'ipregex', '__version__.py'), 'r') as f:
+    exec(f.read(), about)
+
+with open(os.path.join(here, 'README.rst'), 'r') as f:
+    readme = f.read()
 
 setup(
     name='ipregex',
-    version='0.1.2',
-    author='Omer F. Ozarslan',
-    author_email='omer@utdallas.edu',
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
     packages=['ipregex'],
-    url='http://github.com/netmes/ipregex/',
-    license='Apache Software License (Apache 2.0)',
-    description=('Provides regular expression for IPv4/IPv6 addresses.'),
-    long_description=open('README.rst').read(),
+    url=about['__url__'],
+    license=about['__license__'],
+    description=about['__description__'],
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*',
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: System :: Networking',
     ],
 )
