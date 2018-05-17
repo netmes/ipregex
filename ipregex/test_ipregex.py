dcd305f917348e88278836a60aa49cdaed0d0ed4
Omer Ozarslan <omer@utdallas.edu>
2018-05-17 10:04:38 -0500

    Add tests for column errors and leading zeros (#1)

--- ipregex/test_ipregex.py
+++ ipregex/test_ipregex.py
@@ -5,7 +5,6 @@ from . import (
 import pytest
 import itertools
 
-
 @pytest.mark.slow
 def test_ipv4_exhaust_correct_addresses():
     regex = IPv4Regex().regex
@@ -29,7 +28,6 @@ def test_ipv4_exhaust_addresses_with_boundary_segments():
         ip_str = '.'.join(ip)
         assert(regex.match(ip_str) != None)
 
-
 def test_ipv4_manual_correct_addresses():
     regex = IPv4Regex().regex
 
@@ -47,7 +45,6 @@ def test_ipv4_manual_correct_addresses():
     assert(regex.match('250.111.11.1') != None)
     assert(regex.match('1.11.222.250') != None)
     assert(regex.match('250.222.11.1') != None)
-
 
 def test_ipv4_manual_incorrect_addresses():
     regex = IPv4Regex().regex
@@ -124,6 +121,29 @@ def test_ipv6_manual_correct_addresses():
     assert(regex.match('eeee:eeee:eeee:eeee:eeee:eeee:eeee:eeee'))
     assert(regex.match('ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff'))
 
+    # Boundaries without leading zeros
+    assert(regex.match('0:0:0:0:0:0:0:0'))
+    assert(regex.match('f:f:f:f:f:f:f:f'))
+    assert(regex.match('10:10:10:10:10:10:10:10'))
+    assert(regex.match('ff:ff:ff:ff:ff:ff:ff:ff'))
+    assert(regex.match('100:100:100:100:100:100:100:100'))
+    assert(regex.match('fff:fff:fff:fff:fff:fff:fff:fff'))
+    assert(regex.match('1000:1000:1000:1000:1000:1000:1000:1000'))
+    assert(regex.match('ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff'))
+
+    # Boundaries with leading zeros
+    assert(regex.match('00:00:00:00:00:00:00:00'))
+    assert(regex.match('000:000:000:000:000:000:000:000'))
+    assert(regex.match('0f:0f:0f:0f:0f:0f:0f:0f'))
+    assert(regex.match('00f:00f:00f:00f:00f:00f:00f:00f'))
+    assert(regex.match('000f:000f:000f:000f:000f:000f:000f:000f'))
+    assert(regex.match('010:010:010:010:010:010:010:010'))
+    assert(regex.match('0010:0010:0010:0010:0010:0010:0010:0010'))
+    assert(regex.match('0ff:0ff:0ff:0ff:0ff:0ff:0ff:0ff'))
+    assert(regex.match('00ff:00ff:00ff:00ff:00ff:00ff:00ff:00ff'))
+    assert(regex.match('0100:0100:0100:0100:0100:0100:0100:0100'))
+    assert(regex.match('0fff:0fff:0fff:0fff:0fff:0fff:0fff:0fff'))
+
     # Collapsed versions
     assert(regex.match('::'))
     assert(regex.match('::1111:1111:1111:1111:1111:1111:1111'))
@@ -135,7 +155,92 @@ def test_ipv6_manual_correct_addresses():
     assert(regex.match('1111:1111:1111:1111:1111:1111::1111'))
     assert(regex.match('1111:1111:1111:1111:1111:1111:1111::'))
 
-    # With leading zeros
+    assert(regex.match('::0:0:0:0:0:0:0'))
+    assert(regex.match('0::0:0:0:0:0:0'))
+    assert(regex.match('0:0::0:0:0:0:0'))
+    assert(regex.match('0:0:0::0:0:0:0'))
+    assert(regex.match('0:0:0:0::0:0:0'))
+    assert(regex.match('0:0:0:0:0::0:0'))
+    assert(regex.match('0:0:0:0:0:0::0'))
+    assert(regex.match('0:0:0:0:0:0:0::'))
 
-    # Without leading zeros
-    assert(regex.match('0:0:0:0:0:0:0:0'))
+    assert(regex.match('::1:1:1:1:1:1:1'))
+    assert(regex.match('1::1:1:1:1:1:1'))
+    assert(regex.match('1:1::1:1:1:1:1'))
+    assert(regex.match('1:1:1::1:1:1:1'))
+    assert(regex.match('1:1:1:1::1:1:1'))
+    assert(regex.match('1:1:1:1:1::1:1'))
+    assert(regex.match('1:1:1:1:1:1::1'))
+    assert(regex.match('1:1:1:1:1:1:1::'))
+
+    assert(regex.match('::f:f:f:f:f:f:f'))
+    assert(regex.match('f::f:f:f:f:f:f'))
+    assert(regex.match('f:f::f:f:f:f:f'))
+    assert(regex.match('f:f:f::f:f:f:f'))
+    assert(regex.match('f:f:f:f::f:f:f'))
+    assert(regex.match('f:f:f:f:f::f:f'))
+    assert(regex.match('f:f:f:f:f:f::f'))
+    assert(regex.match('f:f:f:f:f:f:f::'))
+
+    # Mixed
+    assert(regex.match('0:01:022:0333:a:bb:ccc:ddd'))
+    assert(regex.match('0:01:022:0333:a:bb:ccc::'))
+    assert(regex.match('0:01:022:0333:a::ccc:ddd'))
+
+def test_ipv6_column_errors():
+    regex = IPv6Regex().regex
+
+    assert(not regex.match(':'))
+    assert(not regex.match(':::'))
+    assert(not regex.match('::::'))
+    assert(not regex.match(':::::'))
+    assert(not regex.match('::::::'))
+    assert(not regex.match(':::::::'))
+    assert(not regex.match('::::::::'))
+    assert(not regex.match(':::::::::'))
+    assert(not regex.match('::::::::::'))
+
+    # Compression with 8 hextets
+    assert(not regex.match('1:1:1:1:1:1:1:1::'))
+    assert(not regex.match('1:1:1:1:1:1:1::1'))
+    assert(not regex.match('1:1:1:1:1:1::1:1'))
+    assert(not regex.match('1:1:1:1:1::1:1:1'))
+    assert(not regex.match('1:1:1:1::1:1:1:1'))
+    assert(not regex.match('1:1:1::1:1:1:1:1'))
+    assert(not regex.match('1:1::1:1:1:1:1:1'))
+    assert(not regex.match('1::1:1:1:1:1:1:1'))
+    assert(not regex.match('::1:1:1:1:1:1:1:1'))
+    assert(not regex.match('11:11:11:11:11:11:11:11::'))
+    assert(not regex.match('11:11:11:11:11:11:11::11'))
+    assert(not regex.match('11:11:11:11:11:11::11:11'))
+    assert(not regex.match('11:11:11:11:11::11:11:11'))
+    assert(not regex.match('11:11:11:11::11:11:11:11'))
+    assert(not regex.match('11:11:11::11:11:11:11:11'))
+    assert(not regex.match('11:11::11:11:11:11:11:11'))
+    assert(not regex.match('11::11:11:11:11:11:11:11'))
+    assert(not regex.match('::11:11:11:11:11:11:11:11'))
+    assert(not regex.match('111:111:111:111:111:111:111:111::'))
+    assert(not regex.match('111:111:111:111:111:111:111::111'))
+    assert(not regex.match('111:111:111:111:111:111::111:111'))
+    assert(not regex.match('111:111:111:111:111::111:111:111'))
+    assert(not regex.match('111:111:111:111::111:111:111:111'))
+    assert(not regex.match('111:111:111::111:111:111:111:111'))
+    assert(not regex.match('111:111::111:111:111:111:111:111'))
+    assert(not regex.match('111::111:111:111:111:111:111:111'))
+    assert(not regex.match('::111:111:111:111:111:111:111:111'))
+    assert(not regex.match('1111:1111:1111:1111:1111:1111:1111:1111::'))
+    assert(not regex.match('1111:1111:1111:1111:1111:1111:1111::1111'))
+    assert(not regex.match('1111:1111:1111:1111:1111:1111::1111:1111'))
+    assert(not regex.match('1111:1111:1111:1111:1111::1111:1111:1111'))
+    assert(not regex.match('1111:1111:1111:1111::1111:1111:1111:1111'))
+    assert(not regex.match('1111:1111:1111::1111:1111:1111:1111:1111'))
+    assert(not regex.match('1111:1111::1111:1111:1111:1111:1111:1111'))
+    assert(not regex.match('1111::1111:1111:1111:1111:1111:1111:1111'))
+    assert(not regex.match('::1111:1111:1111:1111:1111:1111:1111:1111'))
+
+    # Multiple compressions
+    assert(not regex.match('1::1::1'))
+    assert(not regex.match('1::1::1:1'))
+    assert(not regex.match('1:1:1::1::1'))
+    assert(not regex.match('1::1::'))
+    assert(not regex.match('::1::1'))
